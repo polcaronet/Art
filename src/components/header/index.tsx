58d47d3b180c8f186fcb466a1d6b28f8d2e56a3d
Polcaro Desenvolvedor <polcaronet@gmail.com>
2024-04-19 00:16:54 -0300

    update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -26,7 +26,7 @@ export function Header() {
           <Link to="https://www.facebook.com/damiao.martins.71">
            <FaFacebook size={30} color="#e5e7e7"/>
           </Link>
-          <Link to="https://mail.google.com/mail/u/0/#inbox">
+          <Link to="https://mail.google.com/mail/u/0/#inbox=&damiaomartinspintor@gmail.com">
             <FiMail size={30} color= "#e4be13"/>
           </Link>
            <Link to="https://www.instagram.com/damiao3790">

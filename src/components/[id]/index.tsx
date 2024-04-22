00623cfc0edf859dbd521d283889629269251bcf
Polcaro Desenvolvedor <polcaronet@gmail.com>
2024-04-22 20:38:23 -0300

    update

--- src/components/[id]/index.tsx
+++ src/components/[id]/index.tsx
@@ -1,3 +1,6 @@
+import { FiMail } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
 interface PageDetailProps{
   params: {
     id: string;
@@ -8,6 +11,9 @@ export default function HeaderId({ params}: PageDetailProps){
   return(
     <div>
       <h1>Página detalhes do HeaderId {params.id}</h1>
+      <Link to="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJqZhNrwQMMvsSjrbPTxWKKHQgBDCVfjnsXjvBlnlPpSlpxFLkhNNMZfMzbPKKjpQXfbZkg" className="text-cyan-100" title="damiaomartinspintor@gmail.com">
+            <FiMail size={30} color= "#e4be13"/>
+          </Link>
     </div>
   )
 }

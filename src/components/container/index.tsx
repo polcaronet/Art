5ef8b22e68ed236e74dd27e785c984c93290ba80
polcaronet <polcaronet@gmail.com>
2024-08-03 17:59:53 -0300

    Update Footer

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -5,7 +5,7 @@ export function Container({children}: { children: ReactNode }){
         <div className="w-full max-w-7xl mx-auto px-4 ">
       {children}
        <footer className="bg-zinc-900 flex flex-col items-center justify-center py-3 gap-2 mt-0 rounded-md">
-          <span className="text-white ">@2024 | Design Template | <a href="https://www.linkedin.com/in/anselmo-polcaro-ribeiro-b2a570207">Polcaro</a></span>
+          <span className="text-white ">@2024 | Design Template | <a className="text-blue-600" href="https://www.linkedin.com/in/anselmo-polcaro-ribeiro-b2a570207" target="_blank">Polcaro</a></span>
        </footer>
        </div>
   )

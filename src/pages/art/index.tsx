c295e79fdc47b559b53719e1db616abfb4f17689
polcaronet <polcaronet@gmail.com>
2025-10-05 01:43:51 -0300

    update

--- src/pages/art/index.tsx
+++ src/pages/art/index.tsx
@@ -8,20 +8,20 @@ import { firestore } from '../../services/firebaseConnection'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 
-interface ArtProps{
-    id: string;
+interface ArtProps {
+  id: string;
   name: string;
   city: string;
   year: string;
-    cm: string;
- created: string;
- owner: string;
-   uid: string;
+  cm: string;
+  created: string;
+  owner: string;
+  uid: string;
   images: ImagesArtProps[]
   whatsapp: string;
 }
 
-interface ImagesArtProps{
+interface ImagesArtProps {
   uid: string;
   name: string;
   url: string;
@@ -34,120 +34,120 @@ export function ArtDetail() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    async function loadArt(){
-      if(!id){ return }
+    async function loadArt() {
+      if (!id) { return }
 
       const docRef = doc(firestore, "arts", id)
       getDoc(docRef)
-      .then((snapshot) => {
+        .then((snapshot) => {
 
-        if(!snapshot.data()){
-          navigate("/")
-        }
+          if (!snapshot.data()) {
+            navigate("/")
+          }
 
-       setArt({
-         id: snapshot.id,
-         name: snapshot.data()?.name,
-         year: snapshot.data()?.year,
-         city: snapshot.data()?.city,
-         uid: snapshot.data()?.uid,
-         created: snapshot.data()?.created,
-         cm: snapshot.data()?.cm,
-         whatsapp: snapshot.data()?.whatsapp,
-         owner: snapshot.data()?.owner,
-         images: snapshot.data()?.images,
-       })
-     })
-   }
+          setArt({
+            id: snapshot.id,
+            name: snapshot.data()?.name,
+            year: snapshot.data()?.year,
+            city: snapshot.data()?.city,
+            uid: snapshot.data()?.uid,
+            created: snapshot.data()?.created,
+            cm: snapshot.data()?.cm,
+            whatsapp: snapshot.data()?.whatsapp,
+            owner: snapshot.data()?.owner,
+            images: snapshot.data()?.images,
+          })
+        })
+    }
 
-  loadArt()
+    loadArt()
 
-  }, [id])
+  }, [id, navigate])
 
 
   useEffect(() => {
-  
-   function handleResize(){
-     if(window.innerWidth < 720){
-         setSliderPerView(1); 
-       }else{
-         setSliderPerView(2);
-       }
-     }
 
-     handleResize();
+    function handleResize() {
+      if (window.innerWidth < 720) {
+        setSliderPerView(1);
+      } else {
+        setSliderPerView(2);
+      }
+    }
 
-     window.addEventListener("resize", handleResize)
+    handleResize();
 
-     return() => {
-       window.removeEventListener("resize", handleResize)
-     }
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
 
   }, [])
-  
- 
+
+
   return (
     <Container>
-      
-        { art && (
-            <Swiper
-            slidesPerView = {sliderPerView}
-            pagination = {{ clickable: true }}
-            navigation
-          >
-            {art?.images.map( image => (
-                <SwiperSlide key={image.name}>
-                  <img 
-                    src={image.url}
-                    className="w-full h-auto object-cover"
-                  />
-                </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
 
-      { art && (
-      <main className="w-full bg-aqua text-white rounded-lg p-6 my-4">
-        <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
-          <h1 className="font-bold text-3xl ">{art?.name}</h1>
-        </div>
-        <p className="font-bold">Oleo sobre tela</p>
-        
-        <div className="flex w-full gap-6 my-4">
-          <div className="flex flex-col gap-4">
-            <div>
-              <p>Cidade</p>
-              <strong>{art?.city}</strong>
-            </div> 
-            <div>
-              <p>Ano</p>
-              <strong>{art?.year}</strong>
-            </div> 
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <div>
-              <p>Medidas</p>
-              <strong>{art?.cm} cm</strong>
-            </div> 
-          </div>
-
-        </div>
-
-        <strong>Telefone / WhatsApp</strong>
-        <p>{art?.whatsapp}</p>
-        <a
-          href={`https://api.whatsapp.com/send/?type=phone=${art?.whatsapp}&text=Olá vi esse ${art.name} na galeria e fiquei interessado!`}
-          className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium"
+      {art && (
+        <Swiper
+          slidesPerView={sliderPerView}
+          pagination={{ clickable: true }}
+          navigation
         >
-          Preço a Combinar
-          <FaWhatsapp size={26} color="#FFF" />
-        </a>
+          {art?.images.map(image => (
+            <SwiperSlide key={image.name}>
+              <img
+                src={image.url}
+                className="w-full h-auto object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
 
-      </main>
+      {art && (
+        <main className="w-full bg-aqua text-white rounded-lg p-6 my-4">
+          <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+            <h1 className="font-bold text-3xl ">{art?.name}</h1>
+          </div>
+          <p className="font-bold">Oleo sobre tela</p>
+
+          <div className="flex w-full gap-6 my-4">
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>Cidade</p>
+                <strong>{art?.city}</strong>
+              </div>
+              <div>
+                <p>Ano</p>
+                <strong>{art?.year}</strong>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>Medidas</p>
+                <strong>{art?.cm} cm</strong>
+              </div>
+            </div>
+
+          </div>
+
+          <strong>Telefone / WhatsApp</strong>
+          <p>{art?.whatsapp}</p>
+          <a
+            href={`https://api.whatsapp.com/send/?type=phone=${art?.whatsapp}&text=Olá vi esse ${art.name} na galeria e fiquei interessado!`}
+            className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium"
+          >
+            Preço a Combinar
+            <FaWhatsapp size={26} color="#FFF" />
+          </a>
+
+        </main>
       )}
     </Container>
-    
+
   )
 }
 

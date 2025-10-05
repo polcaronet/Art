dc1e65068bb29366460478180caccc0b36a7b6a7
polcaronet <polcaronet@gmail.com>
2025-10-05 01:42:03 -0300

    header

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,24 +1,24 @@
-import { RegisterOptions, UseFormRegister } from 'react-hook-form'
+import { FieldValues, RegisterOptions, UseFormRegister } from 'react-hook-form'
 
-interface InputProps{
+interface InputProps {
   type: string;
   placeholder: string;
   name: string;
-  register: UseFormRegister<any>;
+  register: UseFormRegister<FieldValues>;
   error?: string;
   rules?: RegisterOptions;
 }
 
 
-export function Input({ name, placeholder, type, register, rules, error }: InputProps){
-  return(
+export function Input({ name, placeholder, type, register, rules, error }: InputProps) {
+  return (
     <div>
       <input
-      className="w-full border-2 rounded-md h-11 outline-none px-2 bg-white text-zinc-800"
-         placeholder={placeholder}
-         type={type}
-         {...register(name, rules)}
-         id={name}
+        className="w-full border-2 rounded-md h-11 outline-none px-2 bg-white text-zinc-800"
+        placeholder={placeholder}
+        type={type}
+        {...register(name, rules)}
+        id={name}
       />
       {error && <p className="my-1 text-red-300">{error}</p>}
     </div>
